10104cf3ab55c3d958f41009ec138cc9cb9dd332
msalafia <msalafia@seamgroup.com>
2022-05-13 21:01:30 -0300

    Agregamos templates

---
+++ docs/tarea01.md
@@ -0,0 +1,9 @@
+# Tarea 1
+
+Usando el ejemplo dado en clase 'ejempo'
+
+1. Crear un listado de productos (nombre|descripcion|categoria|precio) - descripcion no se muestra en la lista
+2. Crear un formulario para agregar productos
+3. Ir a una página para visualizar el producto
+4. Agrear link para volver al menú principal
+5. Agrear link en el menú principal para ir a productos
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
 import express, {Request, Response} from 'express';
+import path from 'path';
 
 const PORT = 5005;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "..", "views"));
+app.set('view engine', 'ejs');
 
 app.get("/saludar", (req: Request, res: Response) => {
     const nombre = req.query.nombre;
     const edad = req.query.edad;
     
     res.send(`<html><body><h1>Hola ${nombre}</h1><p>Tu edad es: ${edad}</p></body></html>`);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.render('pages/home');
+});
+
+app.get("/ejemplo", (req: Request, res: Response) => {
+    res.render("pages/ejemplo/index");
+});
+
+app.get("/ejemplo/crear", (req: Request, res: Response) => {
+    res.render("pages/ejemplo/formulario");
+});
+
+app.post("/ejemplo/guardar", (req: Request, res: Response) => {
+    
+    console.info("-- body", req.body);
+
+    res.redirect("/ejemplo");
+});
+
+app.get("/ejemplo/ver/:nombre", (req: Request, res: Response) => {
+    console.info("---", req.params.nombre)
+    res.render("pages/ejemplo/ver", { nombre: req.params.nombre});
 });
 
 app.listen(PORT, () => {
